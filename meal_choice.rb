65b129c9f0ebe0f67f8a1c7294d3b36e8232cc94
Jordan Ginor <jordan.ginor@gmail.com>
2018-12-31 19:06:51 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,4 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-meal_choice(argument = "meat")
+def meal_choice(argument = "meat")
+end 
